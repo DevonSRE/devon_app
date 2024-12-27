d969d0ed999e17f10e5569f7748f57c02a7f34f3
terejoe <nzeamalunkechinyere@gmail.com>
2024-12-27 23:27:04 +0100

    fixed the texts and hero image

--- src/app/home/hero/page.jsx
+++ src/app/home/hero/page.jsx
@@ -19,18 +19,16 @@ const Hero = () => {
 
 
       {/* Content Container */}
-      <div className="flex justify-center z-10 h-screen">
-        <div className="flex justify-center items-center md:w-[1000px]">
-          <div className="md:p-20 px-4">
+      <div className="md:relative md:block flex justify-center h-screen md:h-fit">
+        <div className="md:absolute md:z-10 md:block flex items-center">
+          <div className="md:py-40 md:pl-24 px-4 max-w-[800px]">
             <h1 className="font-lexend font-semibold md:text-[40px] md:pt-20 text-[28px]">
               Empowering Change Through
               Innovative Solutions
             </h1>
-            <p className="font-lexend font-light text-sm md:mt-10 mt-4">
+            <p className="font-lexend font-light text-sm md:mt-6 mt-4 max-w-[480px]">
               At Devon Technology, we strive to lead the way in developing
-              software
-              <br />
-              that addresses pressing social issues.
+              software that addresses pressing social issues.
             </p>
             <div className="pb-20 flex md:gap-4 gap-2">
               <button className="bg-black border border-white mt-8 md:px-8 px-2 py-3 rounded-lg items-center">
@@ -56,7 +54,7 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div className="md:flex justify-center items-center z-0 hidden">
+        <div className="md:flex justify-end items-center z-0 hidden">
           <Image src={heroImg} alt="An image of the Ecllipse" />
         </div>
       </div>
