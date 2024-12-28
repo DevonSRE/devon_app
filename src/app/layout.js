c7c255c29df8102748fc9b97ae7c5dde04a6b810
terejoe <nzeamalunkechinyere@gmail.com>
2024-12-28 17:14:07 +0100

    dark mode issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${lexend.variable} ${geistSans.variable} antialiased dark:text-white`}>
+      <body className={`${lexend.variable} ${geistSans.variable} antialiased`}>
         <header>
           <Navbar/>
         </header>
