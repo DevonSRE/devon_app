71aacbe74b54ad5aeb594e11de9df3f31d19a977
Godswill Nasara <godswillnlm@gmail.com>
2025-01-02 22:48:10 +0100

    fix: resolve issue with route handler definition

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -1,4 +1,6 @@
 // pages/api/send-email.js
+
+/*
 import mailjet from 'node-mailjet';
 
 const mailjetClient = mailjet.connect(
@@ -6,46 +8,106 @@ const mailjetClient = mailjet.connect(
   process.env.MAILJET_API_PRIVATE_KEY
 );
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { firstname, lastname, email, phone, message } = req.body;
+const mailjet = require('node-mailjet').connect(
+  PUBLIC_KEY,
+  PRIVATE_KEY,
+)
+*/
 
-    if (!firstname || !lastname || !email || !phone || !message) {
-      return res.status(400).json({ error: 'All fields are required.' });
-    }
+const PUBLIC_KEY = process.env.MAILJET_API_PUBLIC_KEY;
+const PRIVATE_KEY = process.env.MAILJET_API_PRIVATE_KEY;
 
-    try {
-      const request = mailjetClient.post('send', { version: 'v3.1' }).request({
-        Messages: [
-          {
-            From: {
-              Email: email, // Replace with your email
-              Name: lastname // Replace with your name
-            },
-            To: [
-              {
-                Email: 'info@devontech.io', // Replace with recipient's email
-                Name: 'Devon Technologies', // Replace with recipient's name
-              },
-            ],
-            Subject: 'New Form Submission',
-            TextPart: `You have a new message from ${lastname} (${email}):\n\n${message}`,
-            HTMLPart: `<p>You have a new message from <strong>${lastname}</strong> (<a href="mailto:${email}">${email}</a>):</p><p>${message}</p>`,
+const Mailjet = require('node-mailjet');
+
+const mailjet = new Mailjet({
+  apiKey: PUBLIC_KEY || 'your-api-key',
+  apiSecret: PRIVATE_KEY || 'your-api-secret'
+});
+
+
+const sendEmail = (email, firstname, lastname, message, phone) => {
+  const request = mailjet
+    .post('send', { version: 'v3.1' })
+    .request({
+      Messages: [
+        {
+          From: {
+            Email: email, // Replace with your email
+            Name: lastname // Replace with your name
           },
-        ],
-      });
+          To: [
+            {
+              Email: 'info@devontech.io', // Replace with recipient's email
+              Name: 'Devon Technologies', // Replace with recipient's name
+            },
+          ],
+          Subject: 'New Form Submission',
+          TextPart: `You have a new message from ${firstname} ${lastname} (${email} - ${phone}):\n\n${message}`,
+          HTMLPart: `<p>You have a new message from <strong>${lastname}</strong> (<a href="mailto:${email}">${email}</a>):</p><p>${message}</p>`,
+        },
+      ],
+    });
 
-      await request;
-      return res.status(200).json({ message: 'Email sent successfully.' });
-    } catch (error) {
-      console.error('Mailjet Error:', error.response?.data || error);
-      return res.status(500).json({ error: 'Failed to send email.' });
-    }
-  } else {
-    res.setHeader('Allow', ['POST']);
-    return res.status(405).json({ error: `Method ${req.method} not allowed` });
+  return request
+    .then((result) => {
+      // console.log(result.body);
+      return result.body;
+    })
+    .catch((err) => {
+      // console.log(err.statusCode);
+      throw new Error(err.statusCode);
+      // return err.statusCode;
+    });
+};
+
+
+// export default async function handler(req, res) {
+export async function POST(req) {
+  const { firstname, lastname, email, phone, message } = await req.json();
+
+  if (!firstname || !lastname || !email || !phone || !message) {
+    return new Response(JSON.stringify({ error: 'All fields are required.' }), {
+      status: 400,
+    });
+    // return res.status(400).json({ error: 'All fields are required.' });
+  }
+
+  try {
+    sendEmail(email, firstname, lastname, message, phone);
+
+    /*
+    const request = mailjet.post('send', { version: 'v3.1' }).request({
+      Messages: [
+        {
+          From: {
+            Email: email, // Replace with your email
+            Name: lastname // Replace with your name
+          },
+          To: [
+            {
+              Email: 'godswill@devontech.io', // Replace with recipient's email
+              Name: 'Devon Technologies', // Replace with recipient's name
+            },
+          ],
+          Subject: 'New Form Submission',
+          TextPart: `You have a new message from ${lastname} (${email}):\n\n${message}`,
+          HTMLPart: `<p>You have a new message from <strong>${lastname}</strong> (<a href="mailto:${email}">${email}</a>):</p><p>${message}</p>`,
+        },
+      ],
+    });
+
+    await request;
+    */
+    return new Response(JSON.stringify({ message: 'Email sent successfully.' }), {
+      status: 200,
+    });
+
+    // return res.status(200).json({ message: 'Email sent successfully.' });
+  } catch (error) {
+    console.error('Mailjet Error:', error.response?.data || error);
+    return new Response(JSON.stringify({ error: 'Failed to send email.' }), {
+      status: 500,
+    });
+    // return res.status(500).json({ error: 'Failed to send email.' });
   }
 }
-
-
-
