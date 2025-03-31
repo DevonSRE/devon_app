3d932217cb88df40ddb1c761930385c972e5d3b6
Godswill Nasara <godswillnlm@gmail.com>
2025-03-31 15:50:44 +0100

    update: modify solutions section
    Add UI elements

--- src/features/homepage/components/solutions.jsx
+++ src/features/homepage/components/solutions.jsx
@@ -6,6 +6,8 @@ import { Separator } from "@/components/ui/separator";
 import { BoltIcon } from "@/components/icons/bolt-icon";
 import { BoltBoardIcon } from "@/components/icons/bolt-board-icon";
 
+import devonPatterns from "/public/svg/devon-d-digital-asset-outline.svg";
+
 const Solutions = () => {
   const headlines = [
     {
@@ -23,41 +25,60 @@ const Solutions = () => {
   ];
 
   return (
-    <div className="max-w-screen-xl mx-auto grid grid-cols-12 gap-10 py-32">
-      <div className="col-span-5 col-start-2">
-        <SectionTag text="Our Solutions" color={'green'} />
-      </div>
-
-      <div className="col-span-5 col-start-2">
-        <SectionTitle className="text-4xl">
-          Transforming Legal Workflows with Tailored <span className="text-green-500">Digital Solutions for Professionals <br /> </span>
-        </SectionTitle>
-      </div>
-
-      <div className="col-span-5 col-start-2">
-        <p className="font-light">
-          Our innovative digital solutions streamline legal processes, enhancing efficiency and accessibility. Empower your practice with technology designed specifically for legal professionals.
-        </p>
-      </div>
-
-      <div className="col-span-5 col-start-2 flex items-start justify-start gap-5 h-fit">
-        {headlines.map((headline, index) => (
-          <Fragment key={headline.id}>
-            <SolutionsHeadline headline={headline} />
-
-            {index + 1 < headlines.length && (<Separator className="h-48" orientation="vertical" />)}
-          </Fragment>
-        ))}
-      </div>
-
-      <div className="col-span-5 col-start-7 row-span-4 row-start-1 bg-gray-200 h-full rounded-xl overflow-hidden">
+    <div className="relative overflow-hidden">
+      <div className="max-w-screen-xl mx-auto grid grid-cols-12 gap-10 py-32">
+        {/* patterns top right */}
         <Image
-          src={solutionsPlaceholder}
-          alt="An image of a boy texting"
-          height={500}
-          width={500}
-          className="object-cover object-center aspect-auto size-full"
+          src={devonPatterns}
+          alt="patterns"
+          height={700}
+          width={700}
+          className="absolute -top-80 -right-96 opacity-15 object-contain object-center -scale-y-100"
         />
+        {/* patterns bottom left */}
+        <Image
+          src={devonPatterns}
+          alt="patterns"
+          height={700}
+          width={700}
+          className="absolute -bottom-72 -left-96 opacity-15 object-contain object-center -scale-x-100 scale-y-100"
+        />
+
+        <div className="col-span-5 col-start-2">
+          <SectionTag text="Our Solutions" color={'green'} />
+        </div>
+
+        <div className="col-span-5 col-start-2">
+          <SectionTitle className="text-4xl">
+            Transforming Legal Workflows with Tailored <span className="text-green-500">Digital Solutions for Professionals <br /> </span>
+          </SectionTitle>
+        </div>
+
+        <div className="col-span-5 col-start-2">
+          <p className="font-light">
+            Our innovative digital solutions streamline legal processes, enhancing efficiency and accessibility. Empower your practice with technology designed specifically for legal professionals.
+          </p>
+        </div>
+
+        <div className="col-span-5 col-start-2 flex items-start justify-start gap-5 h-fit">
+          {headlines.map((headline, index) => (
+            <Fragment key={headline.id}>
+              <SolutionsHeadline headline={headline} />
+
+              {index + 1 < headlines.length && (<Separator className="h-48" orientation="vertical" />)}
+            </Fragment>
+          ))}
+        </div>
+
+        <div className="col-span-5 col-start-7 row-span-4 row-start-1 bg-gray-200 h-full rounded-xl overflow-hidden">
+          <Image
+            src={solutionsPlaceholder}
+            alt="An image of a boy texting"
+            height={500}
+            width={500}
+            className="object-cover object-center aspect-auto size-full"
+          />
+        </div>
       </div>
     </div>
   );
