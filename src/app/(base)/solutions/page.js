1f6962e2540fdc820ae3f876e4faeb6d0a098baf
Godswill Nasara <godswillnlm@gmail.com>
2025-03-31 15:30:59 +0100

    update: finalise implementation of solutions page

--- src/app/(base)/solutions/page.js
+++ src/app/(base)/solutions/page.js
@@ -1,5 +1,19 @@
+import ContactUs from "@/features/homepage/components/contact-us";
+import { Partners } from "@/features/homepage/components/partners";
+import { OurSolutionsHero } from "@/features/homepage/components/solutions/our-solutions-hero";
+import { Products } from "@/features/homepage/components/solutions/products";
+import { ProductsHighlight } from "@/features/homepage/components/solutions/products-highlight";
+
 export default async function Page() {
   return (
-    <div>Solutions page</div>
+    <main>
+      <OurSolutionsHero />
+      <Partners />
+      <ProductsHighlight />
+      <Products />
+      <div className="bg-neutral-100">
+        <ContactUs />
+      </div>
+    </main>
   );
 }
