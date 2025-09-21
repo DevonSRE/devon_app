ebad5484ad5121772985534a6292b23fc4d446b5
Godswill Nasara <godswillnlm@gmail.com>
2025-09-21 13:49:53 +0100

    feat: modify team image and links

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['drive.google.com'],
+    // remotePatterns: [new URL('https://drive.google.com/file/d/**')],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/file/d/**',
+        search: '',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
