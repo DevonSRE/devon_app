992953cef14ab6d3bc2590155bf6a3a773f4270a
Godswill Nasara <godswillnlm@gmail.com>
2025-09-21 16:02:44 +0100

    feat: create policy page

---
+++ src/app/(base)/(policies)/layout.js
@@ -0,0 +1,5 @@
+export default function PolicyLayout({ children }) {
+  return (
+    <main className="min-h-dvh w-full bg-black text-white">{children}</main>
+  );
+}
---
+++ src/app/(base)/(policies)/policy/page.js
@@ -0,0 +1,11 @@
+import PolicyContent from "@/features/homepage/markdown/policy-content.mdx";
+
+export default function Page() {
+  return (
+    <div>
+      <p>policy page</p>
+      <p>Policy content</p>
+      <PolicyContent />
+    </div>
+  );
+}
