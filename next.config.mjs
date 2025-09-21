57e809051c94f86d9da5141d522172e38a6ff7ae
Godswill Nasara <godswillnlm@gmail.com>
2025-09-21 15:45:12 +0100

    feat(config): re-config next.config.mjs to support md|mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,28 @@
+import createMDX from "@next/mdx";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
-    domains: ['drive.google.com'],
+    domains: ["drive.google.com"],
     // remotePatterns: [new URL('https://drive.google.com/file/d/**')],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'drive.google.com',
-        port: '',
-        pathname: '/file/d/**',
-        search: '',
+        protocol: "https",
+        hostname: "drive.google.com",
+        port: "",
+        pathname: "/file/d/**",
+        search: "",
       },
     ],
   },
 };
 
-export default nextConfig;
+// export default nextConfig;
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
