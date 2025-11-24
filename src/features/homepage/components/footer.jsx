7db117e1b9a5d49e94122ea94a6534196a5216e3
Godswill Nasara <godswillnlm@gmail.com>
2025-11-24 17:14:08 +0100

    feat: modify the links to Devon's various social pages

--- src/features/homepage/components/footer.jsx
+++ src/features/homepage/components/footer.jsx
@@ -119,20 +119,26 @@ const socials = [
   {
     id: 1,
     name: "Facebook",
-    url: "https://www.facebook.com/share/1BPLqp3CbU/?mibextid=wwXIfr",
+    url: "https://www.facebook.com/profile.php?id=61571581541306",
     icon: facebook,
   },
   {
     id: 2,
     name: "Instagram",
-    url: "https://www.instagram.com/devon_technologies_ltd?igsh=MXQxczNsMXJ6b29iYQ==",
+    url: "https://www.instagram.com/devon_tech_?igsh=MXcyaTJ5ZHVubnY2dw==",
     icon: instagram,
   },
-  { id: 3, name: "X", url: "https://x.com/DevonTechio", icon: x },
+  {
+    id: 3,
+    name: "X",
+    url: "https://x.com/devontechio?s=21&t=MSYMhx7Evn6oXwhwVmOj6A",
+    icon: x,
+  },
   {
     id: 4,
     name: "LinkedIn",
-    url: "https://www.linkedin.com/company/devon-technologies-ltd/about/?viewAsMember=true",
+    url: "https://www.linkedin.com/company/devon-technologies-ltd/",
+
     icon: linkedin,
   },
   {
