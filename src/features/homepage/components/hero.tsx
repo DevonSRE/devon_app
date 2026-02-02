e5bdf40b09507f7a095f0c329818766da6566719
Godswill Nasara <godswillnlm@gmail.com>
2026-02-02 15:59:41 +0100

    feat: remove countdown and new year components
    now the hero component no longer displays the countdown or the new year
    message components

--- src/features/homepage/components/hero.tsx
+++ src/features/homepage/components/hero.tsx
@@ -5,15 +5,17 @@ import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import { SectionTitle } from "@/components/section-util-tags";
 import { cn } from "@/lib/utils";
+/*
 import { CountdownV2 } from "@/components/countdown-v2";
 import { useState } from "react";
+const [isNewYear, setIsNewYear] = useState(false);
+const targetDate = new Date("2025-12-31T23:59:59");
+*/
 
 const Hero = () => {
-  const [isNewYear, setIsNewYear] = useState(false);
-  const targetDate = new Date("2025-12-31T23:59:59");
   return (
     <div className="bg-[url('/svg/devon-bg-eclipse.svg')] lg:bg-[url('/svg/devon-hero-bg.svg')] bg-neutral-950 bg-cover md:bg-contain bg-left md:bg-right bg-no-repeat w-full relative">
-      {isNewYear ? (
+      {/*isNewYear ? (
         <NewYearCard />
       ) : (
         <CountdownV2
@@ -22,10 +24,10 @@ const Hero = () => {
             setIsNewYear(true);
           }}
         />
-      )}
-      <div className="max-w-screen-sm sm:max-w-screen-md md:max-w-screen-xl xl:max-w-screen-2xl mx-auto min-h-[calc(100dvh-70px)] w-full text-white flex flex-col md:flex-row justify-center md:justify-start items-center">
+      )*/}
+      <div className="max-w-screen-sm sm:max-w-3xl md:max-w-7xl xl:max-w-screen-2xl mx-auto min-h-[calc(100dvh-70px)] w-full text-white flex flex-col md:flex-row justify-center md:justify-start items-center">
         {/* Content Container */}
-        <div className="w-full min-h-full md:relative -translate-y-24 md:-translate-y-16 lg:-translate-y-0">
+        <div className="w-full min-h-full md:relative -translate-y-24 md:-translate-y-16 lg:translate-y-0">
           <div className="w-full h-full">
             <div className="w-full grid grid-cols-1 md:grid-cols-12 gap-1 px-5 md:px-0">
               <div className="col-span-full md:col-span-6 col-start-1 md:col-start-2">
@@ -83,6 +85,7 @@ const Hero = () => {
 
 export default Hero;
 
+/*
 const NewYearCard = () => {
   return (
     <div className="absolute top-0 left-1/2 -translate-x-1/2">
@@ -90,3 +93,4 @@ const NewYearCard = () => {
     </div>
   );
 };
+*/
